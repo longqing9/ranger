c689271bf83e70e3aaccf7980fd75c7ac46fa6ec
Liuhualong <1611304485@qq.com>
2022-08-17 11:08:29 +0800

    kafka的权限控制文件的位置

--- agents-common/scripts/enable-agent.sh
+++ agents-common/scripts/enable-agent.sh
@@ -231,7 +231,7 @@ if [ "${HCOMPONENT_NAME}" = "solr" ]; then
 	chown $install_owner:$install_owner $HCOMPONENT_CONF_DIR
     fi    
 elif [ "${HCOMPONENT_NAME}" = "kafka" ]; then
-    HCOMPONENT_CONF_DIR=${HCOMPONENT_INSTALL_DIR}/config
+    HCOMPONENT_CONF_DIR=${HCOMPONENT_INSTALL_DIR}/ranger-kafka-plugin-impl
 elif [ "${HCOMPONENT_NAME}" = "hadoop" ]; then
     HCOMPONENT_CONF_DIR=${HCOMPONENT_INSTALL_DIR}/etc/hadoop
 elif [ "${HCOMPONENT_NAME}" = "yarn" ]; then
